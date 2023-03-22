6811a4e33bd6118bfb1c816e7fd7cf06d0b83228
b9cv9 <117084905+b9cv9@users.noreply.github.com>
2023-03-22 09:53:25 +0400

    Изменение функции fib(n)
    Теперь при вычислении числа Фибоначчи последовательность чисел начинается не с (1, 1..), а с (0, 1..)

--- program.cpp
+++ program.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include<iostream>
 using namespace std;
 
-int fib(int n){
-    if(n == 0) return 0;
-    if(n == 1 || n == 2) return 1;
-    return fib(n - 1)+fib(n - 2);
+int fib(long long  n) {
+    if (n == 1 || n == 0) return 0;
+    if (n == 2) return 1;
+    return fib(n - 1) + fib(n - 2);
 }
-
-int main(){
-    cout << fib(10) << endl;
+int main() {
+    int n;
+    cout << "Введите n: "; cin >> n;
+    cout << fib(n) << endl;
+    return 0;
 }
 
