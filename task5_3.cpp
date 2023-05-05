5862dabc8d2409a1ed80248297b119b2b816c21c
b9cv9 <emil.kurakin95@mail.ru>
2023-05-05 12:38:10 +0400

    Вставка минимальных чисел. Создать стек, содержащий целые числа. Перед  всеми максимальными числами вставить минимальное число. Порядок следования в результате должен совпадать с порядком следования ввода. Например, было 2 8 4 1 2 8 8 1 2 8 Стало 2 1 8 4 1 2 1 8 1 8 1 2 1 8

---
+++ task5_3.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+const int n = 10;
+
+int main(){
+    setlocale(LC_ALL,"rus");
+    stack <int> q;
+    
+    cout << "Р’РІРµРґРёС‚Рµ " << n << " С‡РёСЃРµР»: " << endl;
+    
+    int max = -999999999;
+    int min = 999999999;
+    
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a > max) max = a;
+        if (a < min) min = a;
+        q.push(a);
+    }
+    
+    stack <int> q2;
+    while (!q.empty()){
+        int a;
+        a = q.top();
+        q2.push(a);
+        if (a == max) q2.push(min);
+        q.pop();
+    }
+    cout<<endl;
+    
+    
+    
+    while (!q2.empty()){
+        int a;
+        a = q2.top();
+        cout<<a<<" ";
+        q2.pop();
+    }
+}
